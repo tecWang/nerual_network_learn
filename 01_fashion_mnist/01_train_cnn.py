a79465dbf66a6d34c98e9a774fc950ce8021c9c7
tecwang <tecwang@139.com>
2018-12-11 10:56:10 +0800

    result visual tool

--- 01_fashion_mnist/01_train_cnn.py
+++ 01_fashion_mnist/01_train_cnn.py
@@ -5,7 +5,9 @@ from keras.models import Model, Sequential
 
 import numpy as np
 import matplotlib.pyplot as plt 
+from result_visual import *
 
+result_path = "./result/cnn-result/"
 
 # load fashion mnist datasets
 fashion_mnist = tf.keras.datasets.fashion_mnist
@@ -22,18 +24,15 @@ print("train_labels", train_labels)     # [9 0 0 ... 3 0 5], corresponds to the 
 
 
 # preview first train image
-plt.figure()
-plt.imshow(train_images[0])
-plt.colorbar()
-plt.savefig("./[Image]first_image_[0-255].png")
+# plt.figure()
+# plt.imshow(train_images[0])
+# plt.colorbar()
+# plt.show()
+# plt.savefig("./[Image]first_image_[0-255].png")
 
 # convert 0-255 to 0-1 and convert to floating point
 train_images = train_images / 255.0
 test_images = test_images / 255.0
-plt.figure()
-plt.imshow(train_images[0])
-plt.colorbar()
-plt.savefig("./[Image]first_image_[0-1].png")
 
 # display the first 25 pictures of the training set
 plt.figure(figsize=(10, 10))
@@ -45,7 +44,7 @@ for i in range(25):
     # plt.imshow(train_images[i])
     plt.imshow(train_images[i], cmap=plt.cm.binary)
     plt.xlabel(class_names[train_labels[i]])
-plt.savefig("./[Image]first_25_training_images.png")
+plt.savefig(result_path + "first_25_training_images.png")
 
 
 # build nn model
@@ -68,37 +67,13 @@ model.compile(loss=keras.metrics.categorical_crossentropy,
     
 model.summary()
 history = model.fit(train_images_more_dim, train_labels_onehot, 
-    batch_size=128, validation_split=0.2, epochs=12, verbose=1)
-model.save('./fashion_mnist_model.h5')
+    batch_size=128, validation_split=0.2, epochs=20, verbose=1)
+model.save(result_path + "fashion_mnist_model.h5")
 
 # evaluate model
 test_loss, test_acc = model.evaluate(test_images_more_dim, test_labels_onthot)
 print('Test accuracy:', test_acc)
 
-print(history.history.keys())
-print(history.history["acc"])
-plt.figure()
-plt.subplot(1, 2, 1)
-plt.plot(history.history['acc'])
-plt.plot(history.history['val_acc'])
-plt.title('Model accuracy')
-plt.xlabel('Epoch')
-plt.ylabel('Accuracy')
-plt.legend(['acc', "val_acc"], loc='upper left')
-
-plt.subplot(1, 2, 2)
-plt.plot(history.history['loss'])
-plt.plot(history.history['val_loss'])
-plt.title('Model loss')
-plt.xlabel('Epoch')
-plt.ylabel('Loss')
-plt.legend(['loss', "val_loss"], loc='upper left')
-plt.savefig("./[Image]acc&loss.png")
-
-# output the structure of model to image
-keras.utils.plot_model(model,
-    to_file='./[Image]model.png',
-    show_shapes=True,
-    show_layer_names=True,
-    rankdir='TB')
-
+# result visual
+acc_loss_paint(history, filepath=result_path + "acc_loss.png")
+model_paint(model, filepath=result_path + "model.png")
---
+++ 01_fashion_mnist/result_visual.py
@@ -0,0 +1,34 @@
+import keras
+import matplotlib.pyplot as plt
+
+
+def model_paint(model, filepath="./model.png"):
+    """
+        when rankdir == "TB", vertical layout can be showed
+    """
+    keras.utils.plot_model(model,
+        to_file=filepath,
+        show_shapes=True,
+        show_layer_names=True,
+        rankdir='TB')
+
+def acc_loss_paint(history, filepath="./acc_loss.png"):
+
+    plt.figure()
+    plt.subplot(1, 2, 1)
+    plt.plot(history.history['acc'])
+    plt.plot(history.history['val_acc'])
+    plt.title('Model accuracy')
+    plt.xlabel('Epoch')
+    plt.ylabel('Accuracy')
+    plt.legend(['acc', "val_acc"], loc='lower right')
+
+    plt.subplot(1, 2, 2)
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.title('Model loss')
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss')
+    plt.legend(['loss', "val_loss"], loc='lower right')
+    plt.tight_layout()
+    plt.savefig(filepath)
